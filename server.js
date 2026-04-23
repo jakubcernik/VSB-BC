8db42ce91ff76cbf51bac4b0a0005d0498658ff0
Jakub Černík <jakubcernik15@gmail.com>
2026-04-23 13:09:56 +0200

    converted static html to ejs

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const { algorithms } = require('./data/algorithms.json');
 
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+app.get('/', (req, res) => res.render('index', { algorithms }));
+
+algorithms.forEach(algo => {
+    app.get(algo.route, (req, res) => res.render(algo.id, { algo }));
+    app.get(`${algo.route}/theory`, (req, res) => res.render(`${algo.id}-theory`, { algo }));
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server běží na portu ${PORT}`));
+app.listen(PORT, () => console.log(`Server běží na http://localhost:${PORT}`));
